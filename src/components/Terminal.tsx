319a2e13fcc2ab4d3c61205e11338e07258fe000
vcolavin <vcolavin@gmail.com>
2018-11-04 21:35:15 -0800

    make terminal slightly interactive

--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -1,18 +1,61 @@
 import React from 'react';
+import uuid from '../utils/uuid';
 
-export default class Terminal extends React.Component {
+interface IBufferItem {
+	content: string;
+	id: string;
+}
+
+interface IState {
+	bufferItems: IBufferItem[];
+}
+
+interface IProps {}
+
+export default class Terminal extends React.Component<IProps, IState> {
+	constructor(props) {
+		super(props);
+	}
+
+	state: IState = {
+		bufferItems: [
+			{ content: 'test1', id: uuid() },
+			{ content: 'test2', id: uuid() }
+		]
+	};
+
+	private inputEl: HTMLInputElement = null;
+
 	render() {
 		return (
 			<div className="terminal">
-				<ul className="terminal-buffer">
-					<li>here's some text</li>
-					<li>here's some text</li>
-					<li>here's some text</li>
-					<li>here's some text</li>
-					<li>here's some text</li>
-					<li>here's some text</li>
+				<ul className="terminal-buffer-list">
+					{this.state.bufferItems.map(bufferItem => (
+						<li
+							className="terminal-buffer-list-item"
+							key={bufferItem.id}
+						>
+							{bufferItem.content}
+						</li>
+					))}
 				</ul>
-				<input className="terminal-input" type="text" />
+				<form
+					onSubmit={e => {
+						e.preventDefault();
+						console.log(this.inputEl.value);
+					}}
+				>
+					<input
+						ref={e => {
+							if (e) {
+								e.focus();
+								this.inputEl = e;
+							}
+						}}
+						className="terminal-input"
+						type="text"
+					/>
+				</form>
 			</div>
 		);
 	}
