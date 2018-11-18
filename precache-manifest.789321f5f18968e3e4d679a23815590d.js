340d3e92ea97dc8ff1c36497b457c069b1a83892
vcolavin <vcolavin@gmail.com>
2018-11-18 15:20:44 -0800

    build

---
+++ precache-manifest.789321f5f18968e3e4d679a23815590d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "633bb064ca84ca0a869d",
+    "url": "/static/js/main.633bb064.chunk.js"
+  },
+  {
+    "revision": "a0e550aec0946e5b5986",
+    "url": "/static/js/1.a0e550ae.chunk.js"
+  },
+  {
+    "revision": "633bb064ca84ca0a869d",
+    "url": "/static/css/main.1b07ea6f.chunk.css"
+  },
+  {
+    "revision": "5beebd9a0914eb1f057911f0af0fd177",
+    "url": "/index.html"
+  }
+];
