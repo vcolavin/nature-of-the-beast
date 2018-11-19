faa7517f6790c044e5a614aca11fd927dd5daca2
vcolavin <vcolavin@gmail.com>
2018-11-18 22:21:59 -0800

    something slightly more dynamic in location manifest

--- src/locations/LocationManifest.ts
+++ src/locations/LocationManifest.ts
@@ -1,12 +1,14 @@
 import BaseLocation from './BaseLocation';
 import SomebodyOnceLoved from './SomebodyOnceLoved';
-import QuietForest from './QuietForest';
+import { QuietForest } from './QuietForest';
 import ColdCabin from './ColdCabin';
 
-const LocationManifest: { [s: string]: typeof BaseLocation } = {
-	[SomebodyOnceLoved.slug]: SomebodyOnceLoved,
-	[QuietForest.slug]: QuietForest,
-	[ColdCabin.slug]: ColdCabin
-};
+const locations = [SomebodyOnceLoved, QuietForest, ColdCabin];
+
+const LocationManifest: { [s: string]: BaseLocation } = {};
+
+locations.forEach(location => {
+	LocationManifest[location.slug] = location;
+});
 
 export default LocationManifest;
