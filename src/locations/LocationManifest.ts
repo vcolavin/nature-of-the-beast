bb5a0ee8fe4e0f8bbfa2e87c1583580193d97c8a
vcolavin <vcolavin@gmail.com>
2018-11-18 20:20:19 -0800

    get ls working dynamically

--- src/locations/LocationManifest.ts
+++ src/locations/LocationManifest.ts
@@ -1,5 +1,12 @@
 import BaseLocation from './BaseLocation';
+import SomebodyOnceLoved from './SomebodyOnceLoved';
+import QuietForest from './QuietForest';
+import ColdCabin from './ColdCabin';
 
-const LocationManifest: { [s: string]: typeof BaseLocation } = {};
+const LocationManifest: { [s: string]: typeof BaseLocation } = {
+	someone_somebody_once_loved: SomebodyOnceLoved,
+	a_quiet_forest: QuietForest,
+	a_cold_cabin: ColdCabin
+};
 
 export default LocationManifest;
--- src/utilities/Ls.ts
+++ src/utilities/Ls.ts
@@ -1,9 +1,12 @@
 import BaseUtility from './BaseUtility';
+import LocationManifest from '../locations/LocationManifest';
 
 export default class Ls extends BaseUtility {
 	static run(_args: string[], writeToConsole): void {
 		writeToConsole(
-			'a_cold_cabin     a_quiet_forest     someone_somebody_once_loved'
+			Object.keys(LocationManifest)
+				.sort()
+				.join('    ')
 		);
 	}
 }
