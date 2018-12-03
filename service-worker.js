80769efd230b41c9b6ea13844622e37aa2160061
vcolavin <vcolavin@gmail.com>
2018-12-03 11:07:30 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/nature-of-the-beast/precache-manifest.200d541f8b6caf60a21f8f070827c385.js"
+  "/nature-of-the-beast/precache-manifest.bc547b785f17cf41527515e823ddacde.js"
 );
 
 workbox.clientsClaim();
