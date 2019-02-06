75dc6597de9c8d5bda3ec3bd93786bbcadf3aa7d
Rebecca Colavin <colavin@gmail.com>
2019-02-06 13:20:40 -0800

    removal white space

--- src/utilities/Cd.ts
+++ src/utilities/Cd.ts
@@ -5,6 +5,18 @@ export default class Cd extends BaseUtility {
 	private goToPreviousLocation() {
 		const newLocation = store.getState().previousLocationStack[0];
 
+		store.dispatch({
+            type: ActionTypes.POP_LOCATION_STACK
+        });
+
+        if (newLocation) {
+            store.dispatch({
+                type: ActionTypes.SET_LOCATION,
+                value: newLocation
+            });
+        }
+    }
+
     run({ args, writeToConsole }: RunParams): Promise<null> {
         const nullPromise = Promise.resolve(null);
 
@@ -40,16 +52,6 @@ export default class Cd extends BaseUtility {
     command = 'cd';
     helpDescription =
         'Use cd to move to a new location. For example:\n\ncd a_cold_cabin';
-}
-
-		setUrlLocation({
-            location: store.getState().location
-        });
-
-		return nullPromise;
-	}
-
-    window.history.pushState({ path: newUrl }, '', newUrl);
 }
 
 export function setUrlLocation({ location }: { location: string }): void {
