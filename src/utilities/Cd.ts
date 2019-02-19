b52e293bf29a080dc91883dd583fe394d343f565
Vincent Colavin <vincent@periscopic.com>
2019-02-19 14:18:34 -0800

    run prettier on cd

--- src/utilities/Cd.ts
+++ src/utilities/Cd.ts
@@ -2,62 +2,62 @@ import BaseUtility, { RunParams } from './BaseUtility';
 import store, { ActionTypes, getCurrentLocation } from '../store';
 
 export default class Cd extends BaseUtility {
-    private goToPreviousLocation() {
-        const newLocation = store.getState().previousLocationStack[0];
+	private goToPreviousLocation() {
+		const newLocation = store.getState().previousLocationStack[0];
 
-        store.dispatch({
-            type: ActionTypes.POP_LOCATION_STACK
-        });
+		store.dispatch({
+			type: ActionTypes.POP_LOCATION_STACK
+		});
 
-        if (newLocation) {
-            store.dispatch({
-                type: ActionTypes.SET_LOCATION,
-                value: newLocation
-            });
-        }
-    }
+		if (newLocation) {
+			store.dispatch({
+				type: ActionTypes.SET_LOCATION,
+				value: newLocation
+			});
+		}
+	}
 
-    run({ args, writeToConsole }: RunParams): Promise<null> {
-        const nullPromise = Promise.resolve(null);
+	run({ args, writeToConsole }: RunParams): Promise<null> {
+		const nullPromise = Promise.resolve(null);
 
-        if (args[0] == '..') {
-            this.goToPreviousLocation();
-        } else {
-            const newLocation = getCurrentLocation().neighborSlugs.find(
-                (slug: string) => slug === args[0]
-            );
+		if (args[0] == '..') {
+			this.goToPreviousLocation();
+		} else {
+			const newLocation = getCurrentLocation().neighborSlugs.find(
+				(slug: string) => slug === args[0]
+			);
 
-            if (newLocation) {
-                store.dispatch({
-                    type: ActionTypes.PUSH_LOCATION_STACK,
-                    value: store.getState().location
-                });
+			if (newLocation) {
+				store.dispatch({
+					type: ActionTypes.PUSH_LOCATION_STACK,
+					value: store.getState().location
+				});
 
-                store.dispatch({
-                    type: ActionTypes.SET_LOCATION,
-                    value: newLocation
-                });
-            } else {
-                writeToConsole(`invalid location ${args[0]}`);
-            }
-        }
+				store.dispatch({
+					type: ActionTypes.SET_LOCATION,
+					value: newLocation
+				});
+			} else {
+				writeToConsole(`invalid location ${args[0]}`);
+			}
+		}
 
-        setUrlLocation({
-            location: store.getState().location
-        });
+		setUrlLocation({
+			location: store.getState().location
+		});
 
-        return nullPromise;
-    }
+		return nullPromise;
+	}
 
-    command = 'cd';
-    helpDescription =
-        'Use cd to move to a new location. For example:\n\ncd a_cold_cabin';
+	command = 'cd';
+	helpDescription =
+		'Use cd to move to a new location. For example:\n\ncd a_cold_cabin';
 }
 
 export function setUrlLocation({ location }: { location: string }): void {
-    const currentUrl = window.location.href;
-    const newUrl =
-        currentUrl.substring(0, currentUrl.lastIndexOf('/') + 1) + location;
+	const currentUrl = window.location.href;
+	const newUrl =
+		currentUrl.substring(0, currentUrl.lastIndexOf('/') + 1) + location;
 
-    window.history.pushState({ path: newUrl }, '', newUrl);
+	window.history.pushState({ path: newUrl }, '', newUrl);
 }
