74426a6c331ef99e045cc3e7f929cb7fbeba4093
Vincent Colavin <vincent@periscopic.com>
2019-02-19 16:12:50 -0800

    add some test code for working on different voices

---
+++ src/utils/SpeechUtilities.ts
@@ -0,0 +1,46 @@
+// // karen, moira, tessa, Ting-Ting
+
+// const voices = window.speechSynthesis
+// 	.getVoices()
+// 	.filter(voice => voice.name === 'Ting-Ting');
+
+// voices.reduce((memo, voice): Promise<any> => {
+// 	return memo.then(() => {
+// 		const utterance = new SpeechSynthesisUtterance(
+// 			'this is a test statement. look at the fog. look how it is sad.'
+// 		);
+// 		utterance.voice = voice;
+// 		utterance.rate = 0.8;
+// 		utterance.pitch = 0.6;
+
+// 		console.log(voice.name);
+
+// 		window.speechSynthesis.speak(utterance);
+
+// 		return new Promise(resolve => {
+// 			utterance.onend = () => {
+// 				resolve();
+// 			};
+// 		});
+// 	});
+// }, Promise.resolve(null));
+
+const tessa = window.speechSynthesis
+	.getVoices()
+	.filter(voice => voice.name === 'Tessa')[0];
+
+export default function say(text: string): Promise<null> {
+	const utterance = new SpeechSynthesisUtterance(text);
+	utterance.rate = 0.9;
+	utterance.pitch = 0.8;
+
+	utterance.voice = tessa;
+
+	window.speechSynthesis.speak(utterance);
+
+	return new Promise(resolve => {
+		utterance.onend = () => {
+			window.setTimeout(resolve, 250);
+		};
+	});
+}
