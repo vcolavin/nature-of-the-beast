36625b3a9b43d2e933d3a8d90836c74ec5cfaaaf
vcolavin <vcolavin@gmail.com>
2019-03-04 17:53:49 -0800

    make TerminalBuffer into a functional component

--- src/components/TerminalBuffer.tsx
+++ src/components/TerminalBuffer.tsx
@@ -5,16 +5,14 @@ interface Props {
 	terminalHistory: HistoryItem[];
 }
 
-export default class TerminalBuffer extends React.Component<Props, {}> {
-	render() {
-		return (
-			<div className="terminal-buffer">
-				{this.props.terminalHistory.map(item => (
-					<React.Fragment key={item.id}>
-						{item.content} <br />
-					</React.Fragment>
-				))}
-			</div>
-		);
-	}
+export default function TerminalBuffer({ terminalHistory }: Props) {
+	return (
+		<div className="terminal-buffer">
+			{terminalHistory.map(item => (
+				<React.Fragment key={item.id}>
+					{item.content} <br />
+				</React.Fragment>
+			))}
+		</div>
+	);
 }
