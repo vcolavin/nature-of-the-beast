f56fc56dcd3aeb3704dd7454a3a2babba8aa3a63
vcolavin <vcolavin@gmail.com>
2019-04-10 20:33:13 -0700

    do some fun stuff with the voices

--- src/utils/SpeechUtilities.ts
+++ src/utils/SpeechUtilities.ts
@@ -1,11 +1,20 @@
 import store from '../store';
 
+const voices = speechSynthesis.getVoices();
+
 export default function say(text: string): Promise<null> {
 	const utterance = new SpeechSynthesisUtterance(text);
 
 	utterance.rate = 0.9;
 	utterance.pitch = 0.8;
 	utterance.volume = store.getState().soundOn ? 1 : 0;
+
+	utterance.voice = voices[0];
+
+	if (text[0] === '"' && text[text.length - 1] === '"') {
+		utterance.voice =
+			voices.filter(voice => voice.name === 'Karen')[0] || voices[0];
+	}
 
 	window.speechSynthesis.speak(utterance);
 
