d56b806bb7847d29b335eef50342190ff5c7e21b
vcolavin <vcolavin@gmail.com>
2019-04-16 11:56:38 -0700

    give some feedback for sound

--- src/utilities/Mute.ts
+++ src/utilities/Mute.ts
@@ -2,8 +2,15 @@ import BaseUtility, { PrivateRunParams } from './BaseUtility';
 import store, { ActionTypes } from '../store';
 
 export default class Mute extends BaseUtility {
-	_run({  }: PrivateRunParams): Promise<null> {
+	_run({ output }: PrivateRunParams): Promise<null> {
 		store.dispatch({ type: ActionTypes.TOGGLE_SOUND });
+
+		output({
+			content: `Sound has been turned ${
+				store.getState().soundOn ? 'on' : 'off'
+			}.`
+		});
+
 		return Promise.resolve(null);
 	}
 
