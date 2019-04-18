90e5070345b25595caf180653fc400d59472b59b
vcolavin <vcolavin@gmail.com>
2019-04-17 19:18:51 -0700

    Updates

--- precache-manifest.4b0e3100c30699af6d69e7cf465b8c36.js
+++ precache-manifest.4b0e3100c30699af6d69e7cf465b8c36.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/nature-of-the-beast/static/js/runtime~main.1a6b0e33.js"
   },
   {
-    "revision": "488be37600354ba728c1",
-    "url": "/nature-of-the-beast/static/js/main.488be376.chunk.js"
+    "revision": "b95e2791199ba95ba0e4",
+    "url": "/nature-of-the-beast/static/js/main.b95e2791.chunk.js"
   },
   {
     "revision": "7f6623982363bd0c1735",
     "url": "/nature-of-the-beast/static/js/1.7f662398.chunk.js"
   },
   {
-    "revision": "488be37600354ba728c1",
+    "revision": "b95e2791199ba95ba0e4",
     "url": "/nature-of-the-beast/static/css/main.fd9724cc.chunk.css"
   },
   {
-    "revision": "08e6799d34e3e7a1fa206c7fe983f288",
+    "revision": "6495dce474ef3fe1868229d71156a7e8",
     "url": "/nature-of-the-beast/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/nature-of-the-beast/precache-manifest.c280faf1cae145f45ac6106dd8faba2d.js"
+  "/nature-of-the-beast/precache-manifest.4b0e3100c30699af6d69e7cf465b8c36.js"
 );
 
 workbox.clientsClaim();
