b734009a375f525a0e04dc379cb805b07139cb4e
vcolavin <vcolavin@gmail.com>
2019-04-20 11:21:23 -0700

    give moira a better voice

--- src/utils/SpeechUtilities.ts
+++ src/utils/SpeechUtilities.ts
@@ -4,8 +4,8 @@ const voices = speechSynthesis
 	.getVoices()
 	.filter(({ lang }) => lang.indexOf('en') >= 0);
 
+// TODO: these voices only exist on Firefox.
 const moira = voices.filter(voice => voice.name === 'Moira')[0] || voices[0];
-
 const victoria =
 	voices.filter(voice => voice.name === 'Victoria')[0] || voices[0];
 
@@ -18,11 +18,12 @@ export default function say(text: string): Promise<null> {
 
 	utterance.pitch = 0.8;
 	utterance.rate = 0.9;
-	utterance.volume = 1;
 
 	utterance.voice = victoria;
 
 	if (text[0] === '"' && text[text.length - 1] === '"') {
+		utterance.pitch = 1;
+		utterance.rate = 1;
 		utterance.voice = moira;
 	}
 
