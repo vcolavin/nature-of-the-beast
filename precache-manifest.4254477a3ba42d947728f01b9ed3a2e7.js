b274aaf85d1e5afbe695c072019d2c232dde84bc
vcolavin <vcolavin@gmail.com>
2019-04-21 15:01:54 -0700

    Updates

---
+++ precache-manifest.4254477a3ba42d947728f01b9ed3a2e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a6b0e3313421462bf5f",
+    "url": "/nature-of-the-beast/static/js/runtime~main.1a6b0e33.js"
+  },
+  {
+    "revision": "35f4179cbfd8a6d2e380",
+    "url": "/nature-of-the-beast/static/js/main.35f4179c.chunk.js"
+  },
+  {
+    "revision": "fa342ab1eb04689b0917",
+    "url": "/nature-of-the-beast/static/js/1.fa342ab1.chunk.js"
+  },
+  {
+    "revision": "35f4179cbfd8a6d2e380",
+    "url": "/nature-of-the-beast/static/css/main.e5b100a9.chunk.css"
+  },
+  {
+    "revision": "e8db11d018d4cfbab6cd859d53f43a5e",
+    "url": "/nature-of-the-beast/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/nature-of-the-beast/precache-manifest.f77af2590c95f1ec75b7f48b4de41f84.js"
+  "/nature-of-the-beast/precache-manifest.4254477a3ba42d947728f01b9ed3a2e7.js"
 );
 
 workbox.clientsClaim();
