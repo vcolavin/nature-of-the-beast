c006664a11b4d255e14ee7620a12be3264d0283e
Vincent Colavin <vcolavin@gmail.com>
2019-10-02 14:11:39 -0700

    make help message a function so that the utility manifest is hydrated

--- src/utilities/Help.ts
+++ src/utilities/Help.ts
@@ -1,39 +1,40 @@
-import BaseUtility, { PrivateRunParams } from './BaseUtility';
-import { UtilityManifest, ExtendedUtilityManifest } from './UtilityManifest';
+import BaseUtility, { PrivateRunParams } from "./BaseUtility";
+import { UtilityManifest, ExtendedUtilityManifest } from "./UtilityManifest";
 
 export default class Help extends BaseUtility {
-	protected _run({ output, args }: PrivateRunParams): Promise<null> {
-		if (args.length >= 1) {
-			const util = ExtendedUtilityManifest[args[0]];
-			const description = util
-				? `${util.helpDescription}${
-						util.aliases.length > 0
-							? `\nAliases for this command are: ${[
-									util.command,
-									...util.aliases
-							  ].join(', ')}.`
-							: ''
-				  }`
-				: `I don't know about ${args.join(' ')}.`;
+  protected _run({ output, args }: PrivateRunParams): Promise<null> {
+    if (args.length >= 1) {
+      const util = ExtendedUtilityManifest[args[0]];
+      const description = util
+        ? `${util.helpDescription}${
+            util.aliases.length > 0
+              ? `\nAliases for this command are: ${[
+                  util.command,
+                  ...util.aliases
+                ].join(", ")}.`
+              : ""
+          }`
+        : `I don't know about ${args.join(" ")}.`;
 
-			output({ content: description });
-		} else {
-			output({
-				content: this.helpMessage
-			});
-		}
+      output({ content: description });
+    } else {
+      output({
+        content: this.helpMessage()
+      });
+    }
 
-		return Promise.resolve(null);
-	}
+    return Promise.resolve(null);
+  }
 
-	command = 'help';
-	helpDescription = 'Use help to make sense of the world.';
+  command = "help";
+  helpDescription = "Use help to make sense of the world.";
 
-	helpMessage = `\
+  helpMessage = () => `\
 The Nature of the Beast
 A work of interactive fiction by Vincent Colavin.
 
-Content warning: This story depicts an emotionally abusive parent-child relationship. Descriptions are spoken out loud by your browser by default. Type "mute" to toggle the audio.
+Content warning: This story depicts an emotionally abusive parent-child relationship.
+Descriptions are spoken out loud by your browser by default. Type "mute" to toggle the audio.
 
 Here are some basic commands:
 - List nearby places and objects by typing "list". Places are green, and items are blue.
@@ -44,8 +45,8 @@ Here are some basic commands:
 - To view this message at any time, type "help".
 
 The complete list of available commands is: ${Object.keys(UtilityManifest).join(
-		', '
-	)}.
+    ", "
+  )}.
 
 To get more details for a command, type "help <command>".
 `;
